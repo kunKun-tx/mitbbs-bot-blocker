55ae57895baf1ad64ba647e3dd64c0f78b0eae8c
KuN <kun@geotricks.net>
2017-04-23 12:52:29 -0500

    bump version to v0.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mitbbs-bot-blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Manages and blocks bot generated content. Inspired by Smalltalk80's original GM script, http://userscripts-mirror.org/scripts/review/78633
 // @author       术版小吃
 // @match        http://www.mitbbs.com/*
